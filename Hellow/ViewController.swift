67fb6a4b79b079ec52e32ad93755ebbe6e45932a
FantasyYuna <finalfantasy.yuna@gmail.com>
2019-02-23 09:32:34 +0800

    Xcode 10.1版本
    基本Hellow World
    
    preferredStyle: UIAlertControllerStyle.Alert被取代成
    preferredStyle: UIAlertController.Style.alert
    
    style: UIAlertActionStyle.default被取代成
    style: UIAlertAction.Style.default
    
    self.presentViewController被取代成self.present

--- Hellow/ViewController.swift
+++ Hellow/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func showMessage(){
+        let alertController = UIAlertController(title: "Welcome To My First App", message: "Hellow", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+        
+        
+    }
 
 }
 

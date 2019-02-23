7e7c6f1264a46ef7c611ff3c39b8754b1fc8a8d0
FantasyYuna <finalfantasy.yuna@gmail.com>
2019-02-23 08:13:06 +0800

    Initial Commit

---
+++ Hellow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hellow
+//
+//  Created by Fantasy on 2019/2/23.
+//  Copyright © 2019 Fantasy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
